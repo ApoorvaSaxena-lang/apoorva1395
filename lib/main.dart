8f5e4daff1f1ea9b076c5e01b596e171101bd76e
Mayank985 <mayankjni536@gmail.com>
2023-02-15 11:23:58 +0530

    Profile fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:attendence_tracker/Database/database.dart';
 import 'package:attendence_tracker/Screens/signup_screen.dart';
 import 'package:flutter/material.dart';
-
+//sdgfnsfjbngfng
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   MyDatabase.initDb();
